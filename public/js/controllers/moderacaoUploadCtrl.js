b4f1ea78eb18771a94b8ae50e4b51cc5afe6498b
Vytor Calixto <vytorcalixto@gmail.com>
2015-05-24 22:16:28 +0000

    uploadCtrl.js renomeada para moderacaoUploadCtrl

---
+++ public/js/controllers/moderacaoUploadCtrl.js
@@ -0,0 +1,22 @@
+angular.module('farol.moderacao.upload', ['ui.router'])
+
+.config(['$stateProvider', function ($stateProvider){
+    $stateProvider
+    .state('moderacao.upload',{
+        url: '/uploads',
+        templateUrl: 'templates/moderacao/uploads.html',
+        controller: 'UploadCtrl'
+    });
+}])
+
+.controller('UploadCtrl', ['$scope', '$http', function ($scope, $http){
+    $scope.uploads = [];
+    atualizarUploads();
+    
+    function atualizarUploads(){
+        $http.get('http://pet.inf.ufpr.br/farol/api/v1/arquivos/status/pendente')
+        .success(function (data, status){
+            $scope.uploads = data;
+        });
+    }
+}]);
